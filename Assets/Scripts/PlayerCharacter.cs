d2b7b78e460a668020a5e18db74f6e87f684acd4
Tmac4930 <32606108+Tmac4930@users.noreply.github.com>
2018-09-26 15:15:05 -0500

    Edits to PlayerCharacter Script

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
+    [SerializeField] // expose to the engine without making class/ veriable public
+    private int lives = 3;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello");
+    [SerializeField]
+    private string name = "Mario";
+
+    [SerializeField]
+    private float jumpHeight = 5, speed= 5;
+
+    private bool haskey;
+
+    private bool isOnGround;
+
+    
+	
+    // Use this for initialization
+	void Start ()
+    {
+        //  Debug.Log("Hello");
+        // transform.Translate(0, -1, 5);// using physics, so dont use trainsform. translate.
+       
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	//Horizontal	
 	}
+
+    void Move()
+    {
+
+    }
 }
