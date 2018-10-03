8bb183dd90c706222e028d80e8d0822616d85ade
Tmac4930 <32606108+Tmac4930@users.noreply.github.com>
2018-10-03 15:12:49 -0500

    Physics and friction added

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5;
+    private float jump, maxSpeed, accelerationForce = 5;
 
     [SerializeField]
     private Rigidbody2D rb2D;
 
     private bool isOnGround;
     private float horizontalInput;
-
+    private void Start()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+       
     }
 
 private void FixedUpdate()
     {
-        rb2D.AddForce(Vector2.right * horizontalInput * speed);
+        rb2D.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVolocity = rb2D.velocity;
+        clampedVolocity.x = Mathf.Clamp(rb2D.velocity.x, -maxSpeed, maxSpeed);
     }
 }    
