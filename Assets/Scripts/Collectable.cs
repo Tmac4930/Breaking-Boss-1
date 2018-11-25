7c5c1c1a6911b0b29004ca9975a3bf1b5c4f7758
Tmac4930 <32606108+Tmac4930@users.noreply.github.com>
2018-11-24 22:57:48 -0600

    CoinCountTest
    Coin counter created for pickups.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private int coinCount;
+
+   private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+    if (collision.gameObject.CompareTag("Player"))
+        {
+            debug.log("coinCount:" + coinCount);
+            coinCount++;
+            Destroy(gameObject, audioSource.clip.length);
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            audioSource.Play();
+        }
+
+    }
+}
+ 
